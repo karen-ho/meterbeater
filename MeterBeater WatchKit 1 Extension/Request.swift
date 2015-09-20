85fd62a54c719a0fcc839362e969548c14458621
Dan Kim <dan@symphonycommerce.com>
2015-09-20 07:12:38 -0700

    Watch App finishing touches. Added integration with server. Added a slightly better number input.

--- MeterBeater WatchKit 1 Extension/Request.swift
+++ MeterBeater WatchKit 1 Extension/Request.swift
@@ -10,12 +10,12 @@ import Foundation
 import CoreLocation
 
 class Request {
-    var timeInMinutes: Float
+    var timeInMinutes:Int
     var latitude: CLLocationDegrees
     var longitude: CLLocationDegrees
     var coordinate: CLLocationCoordinate2D
     
-    init(timeInMinutes: Float, latitude: CLLocationDegrees, longitude: CLLocationDegrees, coordinate: CLLocationCoordinate2D) {
+    init(timeInMinutes: Int, latitude: CLLocationDegrees, longitude: CLLocationDegrees, coordinate: CLLocationCoordinate2D) {
         self.timeInMinutes = timeInMinutes
         self.latitude = latitude
         self.longitude = longitude
--- MeterBeater WatchKit 1 Extension/StartMeterController.swift
+++ MeterBeater WatchKit 1 Extension/StartMeterController.swift
@@ -11,16 +11,44 @@ import Foundation
 
 class StartMeterController: WKInterfaceController, CLLocationManagerDelegate {
     
-    var time: Float = 30
+    var time:Int = 30
     var latitude = 0
     var longitude = 0
     
+    //MARK: - Number Slider Code
+    var sliderValue:Int = 30
+    var maxVal:Int = 1440
+    var minVal:Int = 30
+    var increment:Int = 30
+    
     var isRequestingLocation = false
+    
+    let requestURL = "http://172.20.10.3:8080/request"
     let manager = CLLocationManager()
     
     @IBOutlet weak var meterTimer: WKInterfaceTimer!
-    @IBOutlet weak var meterSlider: WKInterfaceSlider!
     @IBOutlet weak var startButton: WKInterfaceButton!
+    
+    @IBAction func meterSliderDidIncrease() {
+        sliderValue = sliderValue + increment
+        
+        if sliderValue > maxVal {sliderValue = maxVal} //enforce upper boundary
+        updateLabel()
+    }
+    
+    @IBAction func meterSliderDidDecrease() {
+        sliderValue = sliderValue - increment
+        if sliderValue < minVal {sliderValue = minVal} //enforce lower boundary
+        updateLabel()
+    }
+    
+    func updateLabel(){
+        self.time = sliderValue
+        
+        let interval: NSTimeInterval = Double((sliderValue + 1) * 60)
+        meterTimer.setDate(NSDate(timeIntervalSinceNow: interval))
+    }
+    
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -40,6 +68,63 @@ class StartMeterController: WKInterfaceController, CLLocationManagerDelegate {
         super.didDeactivate()
     }
     
+    @IBAction func startMeter() {
+        //TODO: Send Request Here
+        let query = self.requestURL
+        let url: NSURL = NSURL(string: query)!
+        
+        /** Worried about GPS Sensitivity at Pier 70. Will hard-code coordinates for presentation, just in case.
+        let lat: Double = Double((self.location?.latitude)!)
+        let long: Double = Double((self.location?.longitude)!)
+        */
+        
+        let lat: Double = 37.7586
+        let lon: Double = -122.3844
+
+        let request = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 60.0)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let params = ["customerId":"1", "lat":lat, "lon":lon, "minutes": self.time]
+        
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(params, options: [])
+        } catch {
+            print(error)
+            request.HTTPBody = nil
+        }
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            if((error) != nil) {
+                NSLog(error!.localizedDescription)
+            } else {
+                if data != nil {
+                    if let csv = NSString(data: data!, encoding: NSUTF8StringEncoding ) {
+                        NSLog("\(csv)")
+                    }
+                }
+            }
+        })
+        task.resume()
+        
+        
+        // do http request and if successful
+        hasMeterTimer = true
+        let latDegrees = CLLocationDegrees(lat)
+        let lonDegrees = CLLocationDegrees(lon)
+        let coordinates = CLLocationCoordinate2D(latitude: latDegrees, longitude: lonDegrees)
+        let requestDTO: Request = Request(timeInMinutes: self.time, latitude: latDegrees, longitude: lonDegrees, coordinate: coordinates)
+        
+        
+        
+        WKInterfaceController.reloadRootControllersWithNames(["CurrentMeter"], contexts: [requestDTO])
+    }
+    
+    
+    /** Worried about GPS sensitivity at Pier 70. Will hard-code coordinates for presentation, just in case.
     @IBAction func startMeter() {
         guard !isRequestingLocation else {
             manager.stopUpdatingLocation()
@@ -63,6 +148,7 @@ class StartMeterController: WKInterfaceController, CLLocationManagerDelegate {
         default:
             NSLog("Some undermined status")
         }
+
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -115,11 +201,5 @@ class StartMeterController: WKInterfaceController, CLLocationManagerDelegate {
             }
         }
     }
-    
-    @IBAction func sliderAction(value: Float) {
-        self.time = value - 1
-        
-        let interval: NSTimeInterval = Double(value*60)
-        meterTimer.setDate(NSDate(timeIntervalSinceNow: interval))
-    }
+    */
 }
